9eb4664792191fea7b0b3760d8add2c99fcca47a
Abdekrahman Khakifa <bodakhalifa707@gmail.com>
2025-11-09 23:30:12 +0200

    inial commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { useState, useEffect } from "react";
+import CoinCard from "./components/CoinCard";
+const API_URL =
+  "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false";
+
+const App = () => {
+  const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchCoins = async () => {
+      try {
+        const res = await fetch(API_URL);
+        if (!res.ok) throw new Error("Failed to fetch data");
+        const data = await res.json();
+        console.log(data);
+        setCoins(data);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchCoins();
+  }, []);
+
+  return (
+    <div>
+      <h1>🚀 Crypto Dash</h1>
+      {loading && <p>Loading...</p>}
+      {error && <p>Error: {error}</p>}
+      {!loading && !error && (
+        <main className="grid">
+          {coins.map((coin) => (
+            <CoinCard key={coin.id} coin={coin} />
+          ))}
+        </main>
+      )}
+    </div>
+  );
+};
+
+export default App;
